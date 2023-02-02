86d24daeb66dfa2d27ef80a8fea7f1d9497b67cc
Raphaël Malet <malet.raphael@gmail.com>
2023-02-02 22:11:21 +0100

    all tests and bonuses done

--- index.js
+++ index.js
@@ -26,13 +26,43 @@ class SortedList {
     }
   }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      let resultSum = 0;
+      this.items.forEach((element) => (resultSum += element));
+      return resultSum;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let intermediateSum = 0;
+      let average = 0;
+      this.items.forEach((element) => (intermediateSum += element));
+      average = intermediateSum / this.items.length;
+      return average;
+    }
+  }
 }
 
 module.exports = SortedList;
