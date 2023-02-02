dd00bd434c8af290b1b55f23b3270fc5e611e127
Raphaël Malet <malet.raphael@gmail.com>
2023-02-02 21:35:08 +0100

    first three methods solved

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  // add(item) {
+  //   this.items.push(item)
+  //   this.items.sort();
+  //   this.length = this.items.length;
+  // }
 
-  add(item) {}
+  add(item) {
+    let i = 0;
+    while (i < this.items.length && this.items[i] < item) {
+      i++;
+    }
+    this.items.splice(i, 0, item);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
   max() {}
 
